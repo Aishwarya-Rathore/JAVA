2fa8c0d6b9e388e303cca09aed90c8dffaee9b1a
Aishwarya Rathore <aishwaryarathore6264@gamil.com>
2026-05-07 12:35:46 +0530

    arrayList

--- PlacmentTraining/Day_6/SubarraySum.java
+++ PlacmentTraining/Day_6/SubarraySum.java
@@ -1,7 +1,8 @@
 public class SubarraySum {
     public static void main(String[] args) {
          // Q:- find the longest subarray whose sum is less than equal to k in a list of element.
-        //     arr[] = {1,2,1,0,1,1,0,1}    O/P:- 5   k=4   
+        //     arr[] = {1,2,1,0,1,1,0,1}    O/P:- 5   k=4
+          
          
     }
 }
